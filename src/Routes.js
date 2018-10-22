02c8d50d3b167c3c3e9627cf3a85723f6dbb7836
Subhankar Panda <s1panda@ucsd.edu>
2018-10-21 17:46:30 -0700

    Home Page

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import {Switch, Route} from 'react-router-dom';
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Layout from './layouts/Layout';
 
 class Routes extends React.Component {
 
@@ -9,10 +10,19 @@ class Routes extends React.Component {
         super(props);
     }
 
+    withLayout(Child) {
+        return (props) => (
+            <Layout>
+                <Child />
+            </Layout>
+        )
+    }
+
     render() {
         return  (
             <Switch>
-                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/home" component={this.withLayout(HomePage)}/>
+                <Route exact path="/folder/:folderId" component={this.withLayout(HomePage)}/>
             </Switch>
         )
     }
