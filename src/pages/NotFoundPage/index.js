29a668ab263e8cd9cf017fae301d9778040e5a3c
lamkalvin <32418155+lamkalvin@users.noreply.github.com>
2018-11-16 11:01:37 -0800

    Updated 404

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
 
 class NotFoundPage extends React.Component {
     render() {
+        var home = <Link to="/home">here</Link>
         return (
             <div>
-                <div className="container pt-5">
+                {/*<div className="container pt-5">
                         <div className="notfound_errorcode">
                             <h1 className="justify-content-center d-flex p-3">404</h1>
                             <h3 className="justify-content-center d-flex p-3">Page Not Found!</h3>
                             <p> Looks like the page you are looking for doesn't have a blueprint...big yikes...</p>
                         </div>
+                </div>*/}
+                <div className="container mt-5 pt-5">
+                    <div className="justify-content-center d-flex"> <img src="/404ErrorPage.jpg"/></div>
+                    <h3 className="justify-content-center d-flex p-3">Ready to stop making the same face? Click&nbsp;{home}&nbsp;to go to your home page.</h3>
                 </div>
             </div>
         )
